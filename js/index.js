c2aaed9c1e35e00419273e6ac738f5226fd53def
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-04-18 05:33:01 +0900

    [add] 모달창 스크롤 기능 추가

--- js/index.js
+++ js/index.js
@@ -108,6 +108,49 @@ $(function() {
     })
 });
 
+// 전체 강사 목록 띄우기
+$(function() {
+    fetch('http://localhost:8080/instructors').then( function(text) { 
+        text.json().then(function(data) {
+            var text = "";
+            for(var i=0 ; i<data.result.length ; i++) {
+                var instrIdx = data.result[i].instrIdx;
+                var name = data.result[i].name;
+                var experience = data.result[i].experience;
+                var introducing = data.result[i].introducing;
+                var profileImgPath = data.result[i].profileImgPath;
+                var region_list = data.result[i].regions;
+
+                // region_list 배열로 변환
+                let regions = "";
+                for(var j=0 ; j < region_list.length ; j++) {
+                    const separator = ", ";
+                    if (regions.length == 0) {
+                        regions += region_list[j].region;
+                    } else {
+                        regions += separator + region_list[j].region;
+                    }
+                }
+
+                text += '<div id="' + instrIdx +'" class="profile_card">\n';
+                text += '<img class="profile" src="' + profileImgPath + '" alt="' + name +' 프로필 이미지">\n';
+                text += '<div class="name">' + name +'</div>\n';
+                text += '<div class="location">in ' + regions +'</div>\n';
+                text += '<div class="experience">experince : ' + experience + ' years</div>\n';
+                text += '<div class="introducing">\n';
+                text += '<p>' + introducing + '</p>\n';
+                text += '</div>\n';
+                text += '<div class="btn_wrap">\n';
+                text += '<button class="contact">CONTACT</button>\n';
+                text += '</div>\n';
+                text += '</div>';
+                
+            }
+            document.querySelector(".instructors_list").innerHTML = text;
+        })
+    })
+});
+
 
 // // 이메일 형식 검증 함수
 // function email_check( email ) {    
