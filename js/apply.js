6694fedb1357547e0543fdbe3d662e6395fa08d8
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-05-03 16:23:37 +0900

    테스트 배포 위해 커밋

--- js/apply.js
+++ js/apply.js
@@ -14,24 +14,17 @@ $(document).ready(function() {
 });
 
 
-function submit_alert(){
-    alert('Your submission is complete! We\'ll contact you within 24 hours.\n' + 
-    '제출이 완료되었습니다. 24시간 이내로 연락드리겠습니다.');
-}
-
 // 제출 시 지원서 이메일 전송
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
+  const fileInput = document.getElementById("file_upload");
   var myForm = document.getElementById('form');
   var payload = new FormData(myForm);
 
+  const uploaded_file = fileInput.files[0];
 
-  const uploaded_file = $('#file_upload').data;
-  console.log(uploaded_file);
   payload.append("ProfileImg", uploaded_file);
-
-  console.log(payload.get("ProfileImg"));
 
   fetch('http://localhost:8080/instructors/apply', {
       method: 'POST',
@@ -39,11 +32,13 @@ form.addEventListener('submit', (e) => {
     })
     .then(res => res.json())
     .then(function(data) {
+      console.log(data);
+
       if(data.isSuccess) {
         alert("Your submission is complete! \n" + 
         "We'll contact you within 24 hours.\n" + 
         "제출이 완료되었습니다. 24시간 이내로 연락드리겠습니다.");
-        location.href='./index.html';
+        // location.href='./index.html';
       } else {
         alert("We have a problem. Try again from the beginning, or contact the administrator.\n" + 
         "문제가 생겼습니다. 처음부터 다시 시도하거나, 관리자에게 문의하세요.");
@@ -53,16 +48,6 @@ form.addEventListener('submit', (e) => {
   localStorage.clear();
 
 });
-
-const fileInput = document.getElementById("file");
-const handleFiles = () => {
-  const selectedFile = fileInput.files;
-  console.log(selectedFile);
-};
-
-fileInput.addEventListener("change", handleFiles);
-
-
 
 // 이메일 형식 검증 함수
 function email_check( email ) {    
@@ -81,5 +66,3 @@ $("input[type=email]").blur(function(){
 	$(".result-email").text('');
   }
 });
-
-// submitButton.addEventListener('click', submit_alert);
