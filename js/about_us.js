eda02cf90af3e5ea06f2650994b8b2a1057c7948
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-05-03 16:30:29 +0900

    [fix]about_us 문서화 해제

--- js/about_us.js
+++ js/about_us.js
@@ -1,6 +1,6 @@
-fetch('docs/About_Us').then(function(response) {
-    response.text().then(function(text) {
-        $(".content").html(text);
-        console.log(text);
-    })
-})
+// fetch('docs/About_Us.txt').then(function(response) {
+//     response.text().then(function(text) {
+//         $(".content").html(text);
+//         console.log(text);
+//     })
+// })
