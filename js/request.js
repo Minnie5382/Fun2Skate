2450c7deaaee76455c0d54b81f8b24b9ef35d765
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-05-26 22:11:38 +0900

    오류 수정

--- js/request.js
+++ js/request.js
@@ -9,7 +9,7 @@ function handleRequest(url, options) {
       if (!response.ok) {
         // throw new Error('Request failed');
       }
-      return response.json();
+      // return response.json();
     })
     .catch(error => {
       // console.error(error);
@@ -54,7 +54,7 @@ form.addEventListener('submit', (e) => {
           "We'll contact you within 24 hours.\n" + 
           "제출이 완료되었습니다. 24시간 이내로 연락드리겠습니다.");
           location.href='/index.html';
-          e.preventDefault();
+          // e.preventDefault();
         } else {
           alert("We have a problem. Try again from the beginning, or contact the administrator.\n" + 
           "문제가 생겼습니다. 처음부터 다시 시도하거나, 관리자에게 문의하세요.");
