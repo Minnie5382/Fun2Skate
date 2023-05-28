808fb34b100e85f988d05a145d201111600245c7
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-05-29 01:15:04 +0900

    오류 수정

--- js/apply.js
+++ js/apply.js
@@ -11,7 +11,7 @@ function handleRequest(url, options) {
         // alert("We have a problem. Try again from the beginning, or contact the administrator.\n" + 
         // "문제가 생겼습니다. 처음부터 다시 시도하거나, 관리자에게 문의하세요.");
       }
-      return response.text();
+      return response.json();
     })
 }
 
@@ -72,7 +72,7 @@ form.addEventListener('submit', (e) => {
   handleRequest(`${backendServerUrl}/instructors/apply`, requestOptions)
     .then(function(data) {
       e.preventDefault();
-      if(data == "강사 등록 요청이 완료되었습니다.") {
+      if(data.isSuccess) {
         alert("Your submission is complete! \n" + 
         "We'll contact you within 24 hours.\n" + 
         "제출이 완료되었습니다. 24시간 이내로 연락드리겠습니다.");
--- js/request.js
+++ js/request.js
@@ -1,18 +1,33 @@
 var MAX_LTR = 500;
-var backendServerUrl = "https://www.fun2skate.site:8080";
+var domain = "https://www.fun2skate.site:8080";
+
+const backendServerUrl = domain;
 
 // handleREquest 함수 선언
 function handleRequest(url, options) {
+  e.preventDefault();
   return fetch(url, options)
     .then(response => {
+      e.preventDefault();
       if (!response.ok) {
         // e.preventDefault();
         // alert("We have a problem. Try again from the beginning, or contact the administrator.\n" + 
         // "문제가 생겼습니다. 처음부터 다시 시도하거나, 관리자에게 문의하세요.");
       }
-      return response.text();
+      return response.json();
     })
 }
+
+// 메시지 박스 글자수 카운트
+$(document).ready(function() {
+  $('#message_box').on('keyup', function() {
+      $('#letter_count').html($(this).val().length);
+      if($(this).val().length > MAX_LTR) {
+          $(this).val($(this).val().substring(0, MAX_LTR));
+          $('#test_cnt').html(MAX_LTR);
+      }
+  });
+});
 
 // 제출 시 이메일 발송
 form.addEventListener('submit', (e) => {
@@ -27,9 +42,10 @@ form.addEventListener('submit', (e) => {
     };
 
   handleRequest(`${backendServerUrl}/email`, requestOptions)
+    .then(data.json)
     .then(function(data) {
       e.preventDefault();
-      if(data == "강사 등록 요청이 완료되었습니다.") {
+      if(data.isSuccess) {
         alert("Your submission is complete! \n" + 
         "We'll contact you within 24 hours.\n" + 
         "제출이 완료되었습니다. 24시간 이내로 연락드리겠습니다.");
@@ -41,16 +57,5 @@ form.addEventListener('submit', (e) => {
     })
   localStorage.clear();
 
-});
-
-// 메시지 박스 글자수 카운트
-$(document).ready(function() {
-  $('#message_box').on('keyup', function() {
-      $('#letter_count').html($(this).val().length);
-      if($(this).val().length > MAX_LTR) {
-          $(this).val($(this).val().substring(0, MAX_LTR));
-          $('#test_cnt').html(MAX_LTR);
-      }
-  });
 });
 
