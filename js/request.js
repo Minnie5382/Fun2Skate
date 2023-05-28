484a534b6774b4a2d2c840e24f68f5b0326a4d1c
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-05-29 00:46:30 +0900

    오류 수정

--- js/request.js
+++ js/request.js
@@ -12,7 +12,7 @@ function handleRequest(url, options) {
         alert("We have a problem. Try again from the beginning, or contact the administrator.\n" + 
         "문제가 생겼습니다. 처음부터 다시 시도하거나, 관리자에게 문의하세요.");
       }
-      return response.json();
+      return response;
     })
     .catch(error => {
       console.error(error);
